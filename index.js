666e6f993f929413c31b7af52a7de085d9438f65
BlasCasale <casale.blas@live.com>
2023-12-05 13:05:02 -0300

    cambiando el push

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ require('dotenv').config();
 const server = require('./src/server');
 const { PORT } = process.env;
 
-console.log('hola nico')
-
 const app = server.listen(PORT, async () => {
     try {
         await db.sync({ force: true });
