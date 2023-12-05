9f83e30cd46dd1bebef953aacadcfa8108d3eae4
BlasCasale <casale.blas@live.com>
2023-12-05 12:57:39 -0300

    probando los push

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const server = require('./src/server');
 const { PORT } = process.env;
 
+console.log('hola nico')
+
 const app = server.listen(PORT, async () => {
     try {
         await db.sync({ force: true });
