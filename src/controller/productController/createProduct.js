e64e1dffa39e934d05231f078606bbb014ba7d9d
unknown <ngaldos98@gmail.com>
2023-12-07 18:37:33 -0300

    Hotfix createProduct

--- src/controller/productController/createProduct.js
+++ src/controller/productController/createProduct.js
@@ -1,8 +1,8 @@
 const { Product } = require('../../db');
 
 const createProduct = async (data) => {
-    const { name, type, description, price, stock, deleted } = data;
-    const product = await Product.create({ name, type, description, price, stock, deleted });
+    const { name, type, description, price, stock, image, deleted } = data;
+    const product = await Product.create({ name, type, description, price, stock, image, deleted });
     return product;
 };
 
--- src/handler/productHandler/createProductHandler.js
+++ src/handler/productHandler/createProductHandler.js
@@ -2,8 +2,8 @@ const createProduct = require('../../controller/productController/createProduct'
 
 const createProductHandler = async (req, res) => {
     try {
-        const { name, type, description, price, stock, deleted } = req.body;
-        const product = await createProduct({ name, type, description, price, stock, deleted });
+        const { name, type, description, price, stock, image, deleted } = req.body;
+        const product = await createProduct({ name, type, description, price, stock, image, deleted });
         res.status(201).json(product);
     } catch (error) {
         res.status(500).json({ error: error.message });
