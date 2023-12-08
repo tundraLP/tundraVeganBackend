d87be8812d2616c434a2f56e6d99d82dd2ba12fb
unknown <ngaldos98@gmail.com>
2023-12-08 02:01:06 -0300

    hotfix

--- src/handler/userHandler/createUserHandler.js
+++ src/handler/userHandler/createUserHandler.js
@@ -3,7 +3,7 @@ const createUser = require('../../controller/userController/createUser');
 const createUserHandler = async (req, res) => {
     try {
         const { name, lastName, mail, adress, password, image } = req.body;
-        const user = await createUser({ name, lastName, mail, adress, password, imgage })
+        const user = await createUser({ name, lastName, mail, adress, password, image })
         res.status(201).json(user);
     } catch (error) {
         res.status(500).json({ error: error.message });
