ef105ddf4d740b9c0b2954857e5dd53f194abe1c
unknown <ngaldos98@gmail.com>
2023-12-07 22:04:50 -0300

    Hotfix orders

--- src/controller/orderController/deleteOrder.js
+++ src/controller/orderController/deleteOrder.js
@@ -1,14 +1,18 @@
 const { Order } = require('../../db');
 const { Op } = require('sequelize');
 
-const deleteOrder = async (UserId, id) => {
-    const order = await Order.findOne({ where: { [Op.and]: [{ UserId, id }] } });
-
+const deleteOrder = async (id) => {
+    const order = await Order.findOne({paranoid: false, where: {id}});
     if (!order) throw Error('No se ha encontrado la orden. Chequee los datos, por favor.');
+    if (order.deletedAt == null){
+        const aux = order;
+        await order.destroy();
+        return aux;
+    }else{
+        await order.restore({ where: {id: id} });
+        return order;
 
-    const aux = order;
-    order.destroy();
-    return aux;
+    }
 };
 
 module.exports = deleteOrder;
--- src/handler/orderHandler/deleteOrderHandler.js
+++ src/handler/orderHandler/deleteOrderHandler.js
@@ -2,8 +2,8 @@ const deleteOrder = require('../../controller/orderController/deleteOrder');
 
 const deleteOrderHandler = async (req, res) => {
     try {
-        const { UserId, id } = req.body;
-        const order = await deleteOrder(UserId, id);
+        const { id } = req.body;
+        const order = await deleteOrder(id);
         res.status(201).json({ order: order, message: 'Su orden ha sido eliminada.' });
     } catch (error) {
         res.status(500).json({ error: error.message });
