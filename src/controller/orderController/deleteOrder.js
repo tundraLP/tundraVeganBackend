f29c5e92f8c7a84d9859b4931cc22e912489da6a
unknown <ngaldos98@gmail.com>
2023-12-08 02:31:24 -0300

    hotfix delete Handlr/Ctrl message

--- src/controller/orderController/deleteOrder.js
+++ src/controller/orderController/deleteOrder.js
@@ -5,9 +5,8 @@ const deleteOrder = async (id) => {
     const order = await Order.findOne({paranoid: false, where: {id}});
     if (!order) throw Error('No se ha encontrado la orden. Chequee los datos, por favor.');
     if (order.deletedAt == null){
-        const aux = order;
         await order.destroy();
-        return aux;
+        return order;
     }else{
         await order.restore({ where: {id: id} });
         return order;
--- src/handler/orderHandler/deleteOrderHandler.js
+++ src/handler/orderHandler/deleteOrderHandler.js
@@ -4,7 +4,10 @@ const deleteOrderHandler = async (req, res) => {
     try {
         const { id } = req.body;
         const order = await deleteOrder(id);
-        res.status(201).json({ order: order, message: 'Su orden ha sido eliminada.' });
+        if (order.deletedAt == null)
+            res.status(201).json({ order: order, message: 'Su orden ha sido restaurada.' });
+        else
+            res.status(201).json({order: order, message: 'Su orden ha sido eliminada.'});
     } catch (error) {
         res.status(500).json({ error: error.message });
     };
