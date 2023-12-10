cada560f39560d2d31d2eb116febf723067cefa6
BlasCasale <casale.blas@live.com>
2023-12-09 22:33:02 -0300

    Delete en cascada de Favs

--- src/controller/userController/createUser.js
+++ src/controller/userController/createUser.js
@@ -3,13 +3,10 @@ const { User } = require('../../db');
 const createUser = async (data) => {
     const { name, lastName, mail, adress, password, image } = data;
 
-    const [user, boolean] = await User.findOrCreate({
-        where: { mail },
-        defaults: { name, lastName, mail, adress, password, image}
-    });
+    const user = await User.create({ name, lastName, mail, adress, password, image })
+        .catch(() => "Ya existe un usuario creado con este mail.");
 
-    if (!boolean) throw Error('Ya existe un usuario registrado con este mail.');
-    else return user;
+    return user;
 };
 
 module.exports = createUser;
