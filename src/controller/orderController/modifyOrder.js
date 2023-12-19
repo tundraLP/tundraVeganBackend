c09f0c21c67fc8df8840b33dbc51fb493e14b2f6
unknown <ngaldos98@gmail.com>
2023-12-19 18:22:41 -0300

    Throw error si el estado es igual al anterior

--- src/controller/orderController/modifyOrder.js
+++ src/controller/orderController/modifyOrder.js
@@ -4,6 +4,7 @@ const modifyOrder = async (id, state)=>{
     const order = await Order.findOne({where: {id}, paranoid: false});
     if (!order) throw Error(`La orden no existe en la base de datos.`);
     if (order.deletedAt == null){
+        if (order.state === state)  throw Error(`El estado es igual al actual.`);
         const modifyOrder = await Order.update({state}, {where: {id}});
         const modifiedOrder = await Order.findByPk(id);
         return modifiedOrder;
