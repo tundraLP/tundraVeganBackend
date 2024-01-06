e21efdf9baa68c9ccbdca5d710b6f2acf2be0213
BlasCasale <casale.blas@live.com>
2024-01-06 10:38:27 -0300

    cambios en controllers por errores en las busquedas

--- src/controller/favoriteController/deleteFavorite.js
+++ src/controller/favoriteController/deleteFavorite.js
@@ -1,13 +1,13 @@
 const { Favorite } = require('../../db');
 
 const deleteFavorite = async (id) => {
-    const favorite = await Favorite.findOne({ paranoid: false, where: { id } });
+    const favorite = await Favorite.findOne({ where: { id: id }, paranoid: false });
     if (!favorite) throw Error(`No se ha encontrado el favorito con ID: ${id}.`);
     if (favorite.deletedAt == null) {
         await favorite.destroy();
         return favorite;
     } else {
-        await favorite.restore({ where: { id } });
+        await favorite.restore({ where: { id: id } });
         return favorite;
     }
 
