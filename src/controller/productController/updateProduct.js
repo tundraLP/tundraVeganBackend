765ab158cd23101b19ef410afd61a24ee9f2e60d
unknown <ngaldos98@gmail.com>
2024-01-12 02:59:30 -0300

    Modificada la respuesta de updateProduct

--- src/controller/productController/updateProduct.js
+++ src/controller/productController/updateProduct.js
@@ -1,10 +1,20 @@
-const { Product } = require('../../db');
+const { Product, Type} = require('../../db');
 
 const updateProduct = async (data) => {
-    const { name, type, description, price, stock, ProductId } = data;
+    const { name, type, description, price, stock, ProductId, image } = data;
 
-    const product = await Product.update({ name, type, description, price, stock }, { where: { id: ProductId } });
-    return product;
+    const product = await Product.update({ name, type, description, price, stock, image }, { where: { id: ProductId } });
+
+    const aux = await Product.findByPk(ProductId, {
+        include: [{
+          model: Type,
+          attributes: ['id', 'name'],
+        }],
+        attributes: {
+          exclude: ['TypeId'],
+        },
+      });
+    return aux;
 };
 
 module.exports = updateProduct;
--- src/handler/productHandler/updateProductHandler.js
+++ src/handler/productHandler/updateProductHandler.js
@@ -2,8 +2,8 @@ const updateProduct = require('../../controller/productController/updateProduct'
 
 const updateProductHandler = async (req, res) => {
     try {
-        const { name, type, description, price, stock, ProductId } = req.body;
-        const product = await updateProduct({ name, type, description, price, stock, ProductId });
+        const { name, type, description, price, stock, ProductId, image } = req.body;
+        const product = await updateProduct({ name, type, description, price, stock, ProductId, image });
         res.status(201).json(product);
     } catch (error) {
         res.status(500).json({ error: error.message });
