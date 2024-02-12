5d46fbea5a4952f80f326100cc4db6d2eb8fb932
ngaldos <ngaldos98@gmail.com>
2024-02-12 03:33:30 -0300

    Hotfix updateUserHandler

--- src/handler/userHandler/updateUserHandler.js
+++ src/handler/userHandler/updateUserHandler.js
@@ -3,6 +3,8 @@ const updateUser = require('../../controller/userController/updateUser');
 const updateUserHandler = async (req, res) => {
     try {
         const { name, lastName, mail, password, adress, UserId , type, image } = req.body;
+        if ( !name && !lastName && !mail && !password && !adress && !type && !image ) 
+        res.status(404).send(`No hay ningun campo a modificar`);
         const user = await updateUser({ name, lastName, mail, password, adress, UserId, type, image });
         res.status(201).json(user);
     } catch (error) {
