8cb3243d97ea750adcabd753092eca5ca2537381
ngaldos <ngaldos98@gmail.com>
2024-02-18 17:35:28 -0300

    Modificados los handlers y ctrls para no tener borrado logico

--- src/handler/reviewHandler/deleteReviewHandler.js
+++ src/handler/reviewHandler/deleteReviewHandler.js
@@ -4,10 +4,7 @@ const deleteReviewHandler = async (req, res) => {
     try {
         const { id } = req.body;
         const review = await deleteReview(id);
-        if (review.deletedAt == null)
-            res.status(201).json({ review: review, message: 'La reseña fue restaurada con exito.' });
-        else
-            res.status(201).json({ review: review, message: 'La reseña fue eliminada con exito.' });
+        res.status(201).json({ review: review, message: 'La reseña fue eliminada con exito.' });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
