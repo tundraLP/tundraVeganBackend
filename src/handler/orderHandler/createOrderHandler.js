634a328d68d2fcfe9fd88262c53a9bd2fda7ac09
unknown <ngaldos98@gmail.com>
2024-04-01 13:28:12 -0300

    Hotfix return message

--- src/handler/orderHandler/createOrderHandler.js
+++ src/handler/orderHandler/createOrderHandler.js
@@ -5,7 +5,7 @@ const createOrderHandler = async (req, res) => {
         const { state, adress, products, total, UserId } = req.body;
         const date = new Date().toISOString().slice(0, 10);
         const order = await createOrder(state, adress, products, total, UserId, date);
-        res.status(201).json(order)
+        res.status(201).json({message: `La orden fue creada exitosamente.`, order})
     } catch (error) {
         res.status(500).json({ error: error.message });
     };
